f542777a647fbf019fbd8869eff54a51f96f2ab2
Bharathmogaveer <bharathmogaveer01@gmail.com>
2022-07-29 14:11:14 +0530

    add form fields

--- src/App.js
+++ src/App.js
@@ -1,9 +1,64 @@
+import React  from "react";
+import "./App.css";
 
 
 function App() {
+ 
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <div className="receipt_details">Receipt Details</div>
+      <div className="table_contents">
+        <div className="date_container">
+          <div className="container_text">Date</div>
+          <input
+            type="date"
+            placeholder="Enter Date"
+            style={{
+              borderRadius: "4px",
+              border: "1px solid gray",
+              width: "200px",
+            }}
+          />
+        </div>
+        <div className="amount_contents">
+          <div className="container_text">Amount</div>
+          <input
+            type="number"
+            placeholder="Enter Amount(in INR)"
+            style={{
+              borderRadius: "4px",
+              border: "1px solid ",
+              width: "350px",
+            }}
+          />
+        </div>
+        <div className="payment_contents">
+          <div className="container_text">Payment</div>
+          <select
+            style={{
+              borderRadius: "4px",
+              border: "gray 1px solid",
+              width: "200px",
+            }}
+          >
+            <option >Cash</option>
+            <option >Cheque</option>
+            <option >Online</option>
+          </select>
+        </div>
+        <div className="remarks_contents">
+          <div className="remarks">Remarks</div>
+          <input
+            type="text"
+            placeholder="Enter Remarks"
+            style={{
+              borderRadius: "4px",
+              border: "1px solid gray",
+              width: "350px",
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
